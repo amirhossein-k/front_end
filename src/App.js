fe26df5b12b720ccc5405dc443a8040ff87453dc
amirhossein-k <amirhossein.k787878@gmail.com>
2023-04-19 20:04:54 +0330

    front-uploade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useEffect, useState, useRef } from "react";
+import axios from 'axios';
 function App() {
+  const [file, setFile] = useState();
+  const [files, setFiles] = useState([]);
+
+
+  const handleSingleClick = async (e) => {
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append('file',file)
+    const config = {
+      header: {
+        'content-type':'multipart/form'
+      }
+
+    }
+    const {data} = axios.post('localhost:3000/api/uploade/singleFile',formData,config)
+
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={handleSingleClick}>
+        <span className='title'>Uploade Single File</span>
+          <input 
+          
+            type="file"
+            name="file"
+            className="file-input"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+
+          <button type="submit" className="submit">Submit</button>
+        </form>
     </div>
   );
 }
